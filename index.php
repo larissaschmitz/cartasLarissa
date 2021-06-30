faf326d7a5063dee5031bcad26b3475baf931b37
Larissa Schmitz <issaschmitz27@gmail.com>
2021-06-29 22:22:50 -0300

    TAREFA 00001 - PRIMEIRO COMMIT Adição dos arquivos funcao.php, index.php, estilo.css e imagens do baralho.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+    <?php
+
+    include 'funcao.php';
+
+    $titulo = "Jogo de cartas";
+    ?>
+
+    <head> 
+        <meta cherset="UTF-8">
+        <title><?php echo $titulo?></title>
+        <html lang="pt-BR";>
+        <link rel="stylesheet" href="css/estilo.css">
+    </head>
+
+    <body>
+
+    </body>
+</html>
